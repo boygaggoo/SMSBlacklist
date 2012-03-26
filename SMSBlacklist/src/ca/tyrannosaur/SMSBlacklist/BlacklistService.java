a125d375fedf1201db6e70851d417248795828ed
Charlie Liban <charlie@tyrannosaur.ca>
2012-03-26 19:46:22 -0400

    Fixed issue where SMSes were not being filtered on real devices.

--- SMSBlacklist/src/ca/tyrannosaur/SMSBlacklist/BlacklistService.java
+++ SMSBlacklist/src/ca/tyrannosaur/SMSBlacklist/BlacklistService.java
@@ -2,24 +2,36 @@ package ca.tyrannosaur.SMSBlacklist;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.database.Cursor;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.preference.PreferenceManager;
+import android.telephony.SmsMessage;
 import android.util.Log;
 
 public class BlacklistService extends Service {
 
    public static final String ACTION_START_SERVICE = "ca.tyrannosaur.SMSBlacklist.BlacklistService.START_SERVICE";
+   public static final String ACTION_START_AND_FILTER = "ca.tyrannosaur.SMSBlacklist.BlacklistService.START_AND_FILTER";
 
+   public static final String EXTRA_SEEN = "ca.tyrannosaur.SMSBlacklist.BlacklistService.EXTRA_SEEN";
+   public static final String EXTRA_ORIGINAL_INTENT_ACTION = "ca.tyrannosaur.SMSBlacklist.BlacklistService.EXTRA_ORIGINAL_INTENT_ACTION";   
+   
    private static final String TAG = Blacklist.class.getName();
    private static final int ONGOING_ID = 0;
    private static final String PREFS_BLACKLIST_ENABLED_KEY = "blacklistEnabled";
@@ -133,4 +145,160 @@ public class BlacklistService extends Service {
       nmanager.cancel(ONGOING_ID);
    }
 
+   private Pattern buildFilterPattern(Cursor c) {      
+      String affinity = c.getString(
+                           c.getColumnIndexOrThrow(
+                                 Contract.Filters.FILTER_MATCH_AFFINITY));
+
+      String text = c.getString(
+                        c.getColumnIndexOrThrow(
+                                 Contract.Filters.FILTER_TEXT));
+
+      return Contract.buildFilterPattern(
+               text,
+               affinity);
+   }
+
+   private int filterBlacklistedMessages(Context context, Intent intent) {
+      Bundle bundle = intent.getExtras();
+      ContentResolver contentResolver = context.getContentResolver();
+      int cValid = 0;
+      
+      if (bundle != null) {
+         // Get a list of filters
+         Cursor filters = contentResolver.query(
+                  Contract.buildFiltersListUri(),
+                  null,
+                  null,
+                  null,
+                  Contract.Filters.DEFAULT_SORT_ORDER);
+
+         // The raw SMS data
+         Object[] pdus = (Object[]) bundle.get("pdus");
+
+         // The SMS data to put back into the intent
+         byte[][] validPdus = new byte[pdus.length][];
+
+         Log.i(TAG, String.format("Examining %d SMSes...", pdus.length));
+
+         // The invalid messages to write to the log
+         ArrayList<ContentValues> invalidMessages = new ArrayList<ContentValues>();
+         
+         for (int i = 0; i < pdus.length; i++) {
+            SmsMessage message = SmsMessage.createFromPdu((byte[]) pdus[i]);
+            Pattern filterPattern;
+            Matcher filterMatcher;
+            boolean invalidFound = false;
+
+            filters.moveToFirst();
+            while (filters.isAfterLast() == false) {
+               try {
+                  filterPattern = buildFilterPattern(filters);
+                  filterMatcher = filterPattern.matcher(message.getOriginatingAddress());
+
+                  // Stuff the invalid message in an array for later
+                  if (filterMatcher.matches()) {
+                     ContentValues row = new ContentValues();                     
+                     row.put(
+                              Contract.Filters.FILTER_TEXT,
+                              filters.getString(filters.getColumnIndexOrThrow(Contract.Filters.FILTER_TEXT)));
+                     row.put(
+                              Contract.Filters.FILTER_MATCH_AFFINITY,
+                              filters.getString(filters.getColumnIndexOrThrow(Contract.Filters.FILTER_MATCH_AFFINITY)));
+                     row.put(
+                              Contract.Filters.NOTE,
+                              filters.getString(filters.getColumnIndexOrThrow(Contract.Filters.NOTE)));
+                     row.put(
+                              Contract.Logs.DATE_RECEIVED,
+                              message.getTimestampMillis());
+                     row.put(
+                              Contract.Logs.FILTER_ID,
+                              filters.getString(filters.getColumnIndexOrThrow(Contract.Filters._ID)));
+                     row.put(
+                              Contract.Logs.MESSAGE_PDU,
+                              message.getPdu().clone());
+
+                     invalidMessages.add(row);
+                     invalidFound = true;
+                     break;
+                  }
+               }
+               catch (PatternSyntaxException e) {
+                  Log.w(
+                           TAG,
+                           "An invalid pattern was inserted in the database",
+                           e);
+               }
+
+               filters.moveToNext();
+
+            }
+
+            if (!invalidFound)
+               validPdus[cValid++] = (byte[]) pdus[i];
+         }
+
+         filters.close();
+
+         // Stuff the valid pdus back in the intent
+         if (cValid < pdus.length) {
+            byte[][] trimmed = new byte[cValid][];
+            for (int i = 0; i < cValid; i++)
+               trimmed[i] = validPdus[i].clone();
+            bundle.putSerializable(
+                     "pdus",
+                     trimmed);
+         }
+
+         // Write the messages to the log
+         ContentValues[] cv = invalidMessages.toArray(new ContentValues[invalidMessages.size()]);
+         contentResolver.bulkInsert(
+                  Contract.buildLogsListUri(),
+                  cv);
+         
+         Log.i(TAG, String.format("Filtered %d SMS(es)", pdus.length - cValid));         
+      }
+
+      return cValid;
+   }   
+   
+   private void rebroadcast(Intent intent) {
+      intent.setAction("android.provider.Telephony.SMS_RECEIVED");
+      intent.putExtra(EXTRA_SEEN, true);
+      intent.setClassName("com.android.mms", "com.android.mms.transaction.SmsReceiverService");                  
+      getApplicationContext().startService(intent);
+   }
+   
+   /*
+    * Starts the service and possibly filters an SMS at the same time.
+    * Re-broadcasts the original Intent with the new SMSes.
+    * @see android.app.Service#onStartCommand(android.content.Intent, int, int)
+    */
+   public int onStartCommand(Intent intent, int flags, int startId)  {
+      Context context =  getApplicationContext();
+      boolean shouldRebroadcast = true;
+  
+      if (ACTION_START_AND_FILTER.equals(intent.getAction())) {         
+         try {
+            if (apiEndpoint.getEnabled()) {               
+               shouldRebroadcast = filterBlacklistedMessages(context, intent) > 0;                             
+            }            
+         }
+         catch (RemoteException e) {
+            Log.d(TAG, "Could not connect to API");
+         }
+         finally {         
+            if (shouldRebroadcast) {
+               Log.d(TAG, "Rebroadcasting SMS");
+               rebroadcast(intent);
+            }
+            else {
+               Log.d(TAG, "Not rebroadcasting SMS");
+            }
+         }
+      }
+      
+      return super.onStartCommand(intent, flags, startId);
+   }
+   
 }
--- SMSBlacklist/src/ca/tyrannosaur/SMSBlacklist/SMSReceiver.java
+++ SMSBlacklist/src/ca/tyrannosaur/SMSBlacklist/SMSReceiver.java
@@ -1,20 +1,8 @@
 package ca.tyrannosaur.SMSBlacklist;
 
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
-
 import android.content.BroadcastReceiver;
-import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
-import android.telephony.SmsMessage;
 import android.util.Log;
 
 public class SMSReceiver extends BroadcastReceiver {
@@ -22,160 +10,26 @@ public class SMSReceiver extends BroadcastReceiver {
    private static final String TAG = Blacklist.class.getName();
    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
 
-   private Pattern buildFilterPattern(Cursor c) {      
-      String affinity = c.getString(
-                           c.getColumnIndexOrThrow(
-                                 Contract.Filters.FILTER_MATCH_AFFINITY));
-
-      String text = c.getString(
-                        c.getColumnIndexOrThrow(
-                                 Contract.Filters.FILTER_TEXT));
-
-      return Contract.buildFilterPattern(
-               text,
-               affinity);
-   }
-
-   private int filterBlacklistedMessages(Context context, Intent intent) {
-      Bundle bundle = intent.getExtras();
-      ContentResolver contentResolver = context.getContentResolver();
-      int invalidCount = 0;
-
-      if (bundle != null) {
-         // Get a list of filters
-         Cursor filters = contentResolver.query(
-                  Contract.buildFiltersListUri(),
-                  null,
-                  null,
-                  null,
-                  Contract.Filters.DEFAULT_SORT_ORDER);
-
-         // The raw SMS data
-         Object[] pdus = (Object[]) bundle.get("pdus");
-
-         // The SMS data to put back into the intent
-         byte[][] validPdus = new byte[pdus.length][];
-
-         Log.i(TAG, String.format("Examining %d SMSes...", pdus.length));
-
-         // The invalid messages to write to the log
-         ArrayList<ContentValues> invalidMessages = new ArrayList<ContentValues>();
-
-         int cValid = 0;
-         for (int i = 0; i < pdus.length; i++) {
-            SmsMessage message = SmsMessage.createFromPdu((byte[]) pdus[i]);
-            Pattern filterPattern;
-            Matcher filterMatcher;
-            boolean invalidFound = false;
-
-            filters.moveToFirst();
-            while (filters.isAfterLast() == false) {
-               try {
-                  filterPattern = buildFilterPattern(filters);
-                  filterMatcher = filterPattern.matcher(message.getOriginatingAddress());
-
-                  // Stuff the invalid message in an array for later
-                  if (filterMatcher.matches()) {
-                     ContentValues row = new ContentValues();                     
-                     row.put(
-                              Contract.Filters.FILTER_TEXT,
-                              filters.getString(filters.getColumnIndexOrThrow(Contract.Filters.FILTER_TEXT)));
-                     row.put(
-                              Contract.Filters.FILTER_MATCH_AFFINITY,
-                              filters.getString(filters.getColumnIndexOrThrow(Contract.Filters.FILTER_MATCH_AFFINITY)));
-                     row.put(
-                              Contract.Filters.NOTE,
-                              filters.getString(filters.getColumnIndexOrThrow(Contract.Filters.NOTE)));
-                     row.put(
-                              Contract.Logs.DATE_RECEIVED,
-                              message.getTimestampMillis());
-                     row.put(
-                              Contract.Logs.FILTER_ID,
-                              filters.getString(filters.getColumnIndexOrThrow(Contract.Filters._ID)));
-                     row.put(
-                              Contract.Logs.MESSAGE_PDU,
-                              message.getPdu().clone());
-
-                     invalidMessages.add(row);
-                     invalidFound = true;
-                     break;
-                  }
-               }
-               catch (PatternSyntaxException e) {
-                  Log.w(
-                           TAG,
-                           "An invalid pattern was inserted in the database",
-                           e);
-               }
-
-               filters.moveToNext();
-
-            }
-
-            if (!invalidFound)
-               validPdus[cValid++] = (byte[]) pdus[i];
-         }
-
-         filters.close();
-
-         // Stuff the valid pdus back in the intent
-         if (cValid < pdus.length) {
-            byte[][] trimmed = new byte[cValid][];
-            for (int i = 0; i < cValid; i++)
-               trimmed[i] = validPdus[i].clone();
-            bundle.putSerializable(
-                     "pdus",
-                     trimmed);
-         }
-
-         // Write the messages to the log
-         ContentValues[] cv = invalidMessages.toArray(new ContentValues[invalidMessages.size()]);
-         contentResolver.bulkInsert(
-                  Contract.buildLogsListUri(),
-                  cv);
-
-         // Prevent this broadcast from propagating if there are no valid
-         // messages
-         if (cValid == 0)
-            abortBroadcast();
-
-         invalidCount = invalidMessages.size();
-      }
-
-      return invalidCount;
-   }
-
    @Override
    public void onReceive(Context context, Intent intent) {
       Log.d(TAG, "Examining new SMSes...");
-      Intent startService = new Intent(BlacklistService.ACTION_START_SERVICE);
+      
+      if (intent != null && SMS_RECEIVED.equals(intent.getAction()) && !intent.hasExtra(BlacklistService.EXTRA_SEEN)) {
+            
+         Log.d(TAG, "Sending new SMSes to service for examination");
 
-      try {
-         // Start the service if it's been killed for some reason
-         context.startService(startService);
-
-         // Bind the already-running service to the API. Since the service is
-         // using IPC, all calls will not be blocking.
-         IBinder service = peekService(context, startService);
-
-         BlacklistApi api = BlacklistApi.Stub.asInterface(service);
-
-         if (api == null)
-            Log.e(TAG, "The service wasn't started in time, so no SMSes are being filtered for safety");
-
-         if (api != null && api.getEnabled() && SMS_RECEIVED.equals(intent.getAction())) {
-            int invalidCount = filterBlacklistedMessages(context, intent);
-            Log.i(TAG, String.format("Filtered %d SMS(es)", invalidCount));
+         intent.setAction(BlacklistService.ACTION_START_AND_FILTER);
+         intent.setComponent(null);
+         
+         try {
+            context.getApplicationContext().startService(intent);
+            abortBroadcast();
          }
+         catch (SecurityException e) {
+            Log.e(TAG, "Not allowed to start BlacklistService");
+         }
+         
       }
-      catch (RemoteException e) {
-         Log.e(TAG, "Couldn't contact BlacklistService");
-      }
-      catch (SecurityException e) {
-         Log.e(TAG, "Not allowed to start BlacklistService");
-      }
-
-      Log.i(TAG, "Finished examining new SMSes");
    }
 
 }
